03139d08e744b1ba3dce0426fe7fa0b18168f326
jbud2099 <jbud2099@gmail.com>
2021-03-09 17:21:09 -0500

    initial push

---
+++ expressionsAndOperators/comparisonOperators.js
@@ -0,0 +1,39 @@
+//Equal To
+//non-strict check for the value alone to match
+
+let equalTo = "3" == 3;
+console.log(equalTo);
+
+//Strict Equals To
+let strictEqualTo = "3" === 3;
+console.log(strictEqualTo);
+
+//Not Equal To
+let notEqualTo = 3 != "3"
+console.log(notEqualTo);
+
+//Strict Not Equal To
+let strictNotEqualTo = 3 !== "3";
+console.log(strictNotEqualTo);
+
+//Greater Than
+let greaterThan = 4 > 7;
+console.log(greaterThan);
+
+//Less Than
+let lessThan = 33 < 100;
+console.log(lessThan);
+
+//Greater Than or Equal To
+let greaterThanOrEqualTo = 3 >= 2
+console.log(greaterThanOrEqualTo)
+
+//Less Than or Equal To
+let lessThanOrEqualTo = 2 <= 3;
+console.log(lessThanOrEqualTo);
+
+//And
+2 + 2 && 3 + 3
+
+//Or
+2 || 2
